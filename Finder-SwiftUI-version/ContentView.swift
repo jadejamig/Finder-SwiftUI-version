6aab5e40674618a5ec5a93b47595ff34f77f50e6
Jade Jamig <ajamig016@gmail.com>
2021-05-29 16:51:58 +0800

    Initial Commit

---
+++ Finder-SwiftUI-version/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finder-SwiftUI-version
+//
+//  Created by Anselm Jade Jamig on 5/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Finder-SwiftUI-version/Finder_SwiftUI_versionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finder_SwiftUI_versionApp.swift
+//  Finder-SwiftUI-version
+//
+//  Created by Anselm Jade Jamig on 5/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Finder_SwiftUI_versionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
