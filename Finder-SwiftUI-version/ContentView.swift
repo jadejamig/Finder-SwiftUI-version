90983d46c0df263c004f8b0231fe9dc9f2169b18
Jade Jamig <ajamig016@gmail.com>
2021-05-29 17:25:24 +0800

    ui adaptation

--- Finder-SwiftUI-version/ContentView.swift
+++ Finder-SwiftUI-version/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var searchBar: SearchBar = SearchBar()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section {
+                    NavigationLink(destination: EmptyView()) {
+                        Label("Home", systemImage: "house")
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        Label("Downloads", systemImage: "arrow.down.circle")
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        Label("Recents", systemImage: "clock")
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        Label("Airdrop", systemImage: "airplayaudio")
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        Label("Apps", systemImage: "folder")
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        Label("Documents", systemImage: "doc")
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: EmptyView()) {
+                        Label("iCloud", systemImage: "icloud")
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        Label("Documents", systemImage: "doc")
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        Label("Desktop", systemImage: "menubar.dock.rectangle")
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: EmptyView()) {
+                        Label("MacBook", systemImage: "laptopcomputer")
+                    }
+                    NavigationLink(destination: EmptyView()) {
+                        Label("iPhone", systemImage: "iphone")
+                    }
+                }
+            }
+            .add(self.searchBar)
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarItems(trailing: Button(action: {}) {Image(systemName: "list.bullet")})
+            .navigationTitle("Finder")
+        }
     }
 }
 
